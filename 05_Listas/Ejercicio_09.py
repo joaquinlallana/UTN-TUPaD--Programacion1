47b132d7ae21893a48858fe8c1eac33cc26270df
Joaquin Lallana <joaquinlallana12@gmail.com>
2026-08-23 11:45:36 -0300

    Arreglo Ejercicio 9
    Arreglo errores en el código:
    -Validación de ingreso de número dentro del rango solicitado.
    -Validación que se haya ingresado un numero y no otro caracter

--- 05_Listas/Ejercicio_09.py
+++ 05_Listas/Ejercicio_09.py
@@ -10,8 +10,33 @@ while condicion :
 
     casilla_libre = False
     while not casilla_libre :
-        fila_seleccionada = int(input("Ingrese la fila (0 a 2): "))
-        columna_seleccionada = int(input("Ingrese la columna (0 a 2): "))
+        fila_valida = False
+        while not fila_valida :
+            try :
+                fila_seleccionada = int(input("Ingrese la fila (0 a 2): "))
+            except ValueError :
+                print("-- Tenés que ingresar un número entero --")
+                continue
+
+            if fila_seleccionada not in range(3) :
+                print("-- La fila debe estar entre 0 y 2 --")
+                continue
+
+            fila_valida = True
+
+        columna_valida = False
+        while not columna_valida :
+            try :
+                columna_seleccionada = int(input("Ingrese la columna (0 a 2): "))
+            except ValueError :
+                print("-- Tenés que ingresar un número entero --")
+                continue
+
+            if columna_seleccionada not in range(3) :
+                print("-- La columna debe estar entre 0 y 2 --")
+                continue
+
+            columna_valida = True
 
         if tablero[fila_seleccionada][columna_seleccionada] == "-" :
             casilla_libre = True
